cd9f9f0adad75d98f5f55f1985596548b02f7b9b
aly <aliatalla93@gmail.com>
2024-04-15 11:59:42 +0200

    import Apollo in index.js

--- my-ecommerce-app/src/index.js
+++ my-ecommerce-app/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
